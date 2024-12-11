c8849762db23cdf39a262f60099d181e29c680f8
AbinStrat <abin.s@strat-agile.com>
2024-12-11 18:48:35 +0530

    Added <file-name> with description

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+$('.hero-banner__wrap').slick({
+    infinite: true,
+    speed: 800,
+    fade: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    cssEase: 'linear',
+    prevArrow: $('.left'),
+    nextArrow: $('.right'),
+    responsive: [
+        {
+          breakpoint: 600,
+          settings: {
+            autoplay: true,
+            autoplaySpeed: 2000,
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            autoplay: true,
+            autoplaySpeed: 2000,
+          }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ]
+  });
